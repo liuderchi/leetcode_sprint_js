f5a4a04638dfc81f44215f76a3d15f1664652c1c
liuderchi <liuderchi@gmail.com>
2017-02-28 14:56:50 +0800

    refine 338: counting bits

--- 338_Counting_Bits.js
+++ 338_Counting_Bits.js
@@ -16,3 +16,28 @@ var countBits = function(num) {
     }
     return result;
 };
+
+// NOTE
+// It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+// Space complexity should be O(n).
+
+// I. consider if carry
+var countBits = function(num) {
+  var ret = [0];
+  for (var i = 1; i < num + 1; i++) {      // linear time
+    var bitUnChangedIndex = i & (i -1);    // reuse bits not affected by carry
+    ret.push(ret[bitUnChangedIndex] + 1);
+    // non-changed part + carry bit  (carry happens)
+    // non-changed part + additional bit  (carry not happenned)
+  }
+  return ret;  // linear space
+}
+
+// II. consider first bit is 1 or 0
+var countBits = function(num) {
+  var ret = [0];
+  for (var i = 1; i < num + 1; i++) {      // linear time
+    ret.push(ret[i >> 1] + i%2 );  // reuse bit sequence (1~N)
+  }
+  return ret;  // linear space
+}
