76caf4f552d14f908449e442667c3eef32a79dba
liuderchi <liuderchi@gmail.com>
2017-02-28 15:16:49 +0800

    refine 292

--- 292_Nim_Game.js
+++ 292_Nim_Game.js
@@ -6,4 +6,11 @@ var canWinNim = function(n) {
     return Boolean(n % 4);  // return (n % 4) !== 0;
     // NOTE any player can consumpt 4 stone each round for sure
     // so game result is determined by last 0~4 stones
+
+
+    // NOTE 4: bound to lose
+    // 5: bound to win by removing 1
+    // 6: bound to win by removing 2
+    // 7: bound to win by removing 3
+    // 8: bound to lose
 };
