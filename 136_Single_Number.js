7e8c167e8270b3cf279ce7c60d726ec2ba4f1015
liuderchi <liuderchi@gmail.com>
2017-03-02 15:18:39 +0800

    refine 136: single number

--- 136_Single_Number.js
+++ 136_Single_Number.js
@@ -14,3 +14,27 @@ var singleNumber = function(nums) {
     }
     return nums[nums.length-1];    // NOTE last element is single
 };
+
+
+var singleNumber_linearTime = function(nums) {
+    if (nums.length === 1){
+        return nums[0];
+    }
+    var bucket = {};
+
+    for (var i = 0; i < nums.length; i++) {
+        if (bucket[nums[i]]){
+            delete bucket[nums[i]];
+        } else {
+            bucket[nums[i]] = true;
+        }
+    }
+    return parseInt(Object.keys(bucket)[0]);
+};
+
+
+var singleNumber_linearTimeNoExtraRAM = function(nums) {
+    return nums.reduce(function(acc, num){
+        return acc^num; // XOR
+    });
+};
