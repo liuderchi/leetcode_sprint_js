3886c7e83db1562a32d33291efad4c4ac4ff5d10
liuderchi <liuderchi@gmail.com>
2017-04-03 12:07:32 +0800

    update 516 longest palidromic subsequence: print subsequence

--- 516_Longest_Palindromic_Subsequence.js
+++ 516_Longest_Palindromic_Subsequence.js
@@ -19,14 +19,26 @@
 var longestPalindromeSubseq = function(s) {
   var n = s.length;
   var dp = [];
+  var track = [];
 
   for (var i = 0; i<n; i++) {
     var tmp = [];
-    for (var x = 0; x<n; x++) tmp.push(0);
+    var _tmp2 = [];
+    for (var x = 0; x<n; x++) {
+      tmp.push(0);
+      _tmp2.push(0);
+    }
     tmp[i] = 1;
     dp.push(tmp);
     // put 1 in vice diagonal
+    track.push(_tmp2);
   }
+
+  // DEFINE track data
+  // 0: undefined
+  // 1: use data from left, cut right char
+  // 2: use data from bottom, cut left char
+  // 3: use data from left-bottom, cut both right and left
 
   for (i = n-1; i >= 0; i--) {
 
@@ -34,16 +46,42 @@ var longestPalindromeSubseq = function(s) {
 
       if (s[i] === s[j]) {
         dp[i][j] = dp[i+1][j-1] + 2;
+        track[i][j] = 3;
       } else {
         dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+        if (dp[i][j-1] > dp[i+1][j]) {
+            track[i][j] = 1;
+        } else {   // less or equal
+            track[i][j] = 2;
+        }
       }
     }
   }
+  var printContent = [];
+  // print string by track http://www.csie.ntnu.edu.tw/~u91029/Palindrome.html#2
+  var print = function(i, j){
+      if (i>j) return;
+      if (i === j) {
+          printContent.push(s[i]);
+      }
+      else if (track[i][j] === 3) {
+          printContent.push(s[i]);
+          print(i+1, j-1);
+          printContent.push(s[i]);
+      } else if (track[i][j] === 1) {  // cut right
+          print(i, j-1);
+      } else { // cut left
+          print(i+1, j);
+      }
+  }
+  print(0, s.length-1);
+  console.log(printContent.join(''));
+
   return dp[0][n-1];
 };
 
 
-// console.log(longestPalindromeSubseq('aba'));        // 3
-// console.log(longestPalindromeSubseq('aaaaa'));      // 5
-console.log(longestPalindromeSubseq('bbbab'));         // 4
-// console.log(longestPalindromeSubseq('abacaba'));    // 7
+console.log(longestPalindromeSubseq('aba'));        // aba 3
+console.log(longestPalindromeSubseq('aaaaa'));      // aaaaa 5
+console.log(longestPalindromeSubseq('bbbab'));      // bbbb 4
+console.log(longestPalindromeSubseq('abacaba'));    // abacaba 7
