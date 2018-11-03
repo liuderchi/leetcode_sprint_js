81457e66f7e515a4412705d58e1b8509054b828e
Te-Chi Liu <liuderchi@gmail.com>
2018-11-03 23:29:56 +0800

    chore(013): cp -r

---
+++ 013-roman-to-integer/solution.test.js
@@ -0,0 +1,10 @@
+import intToRoman from './solution';
+
+test('basic examples for intToRoman()', () => {
+  expect(intToRoman(0)).toBe('');
+  expect(intToRoman(55)).toBe('LV');
+});
+
+test('type of intToRoman is function', () => {
+  expect(typeof intToRoman).toBe('function');
+});
